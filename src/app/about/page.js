bebec10dfed7243f9f5a90862acb02614380a088
ayan-akd <ayankumar.akd@gmail.com>
2025-01-30 22:12:15 +0600

    finished 5 lessons

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <h1 className="text-3xl font-bold">
+            This is about page
+        </h1>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/dashboard/profile/settings/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileSettingsPage = () => {
+    return (
+        <div>
+            <h1>Profile Settings Page</h1>
+        </div>
+    );
+};
+
+export default ProfileSettingsPage;
